cd30fe57a072ea9a14aac8f99aa072e9446302f5
matthew <matthew.almeida@sbcglobal.net>
2023-04-17 13:16:25 -0400

    Added seeder file

---
+++ backend/db/seeders/20230417171223-spotimages.js
@@ -0,0 +1,62 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+options.tableName = 'SpotImages';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        url: '',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: '',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: '',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: '',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: '',
+        preview: true
+      }
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3, 4, 5] }
+    }, {});
+  }
+};
