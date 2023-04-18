94054748cd082d968b628703e0103b9183135c39
matthew <matthew.almeida@sbcglobal.net>
2023-04-18 12:20:40 -0400

    Added get current reviews route

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -2,10 +2,36 @@ const express = require("express");
 const router = express.Router();
 
 const { requireAuth } = require("../../utils/auth");
-const { Spot, SpotImage, Review, User } = require("../../db/models");
+const { Spot, SpotImage, Review, User, ReviewImage } = require("../../db/models");
 const review = require("../../db/models/review");
 const spot = require("../../db/models/spot");
 
+router.get('/current', requireAuth, async (req, res, next) => {
 
+    const reviews = await Review.findAll({
+        where: {
+            userId: req.user.id
+        },
+        attributes: ['id', 'userId', 'spotId', 'review', 'stars'],
+        include: [
+        {
+            model: User,
+            attributes: ['id', 'firstName', 'lastName']
+        },
+        {
+            model: Spot,
+            attributes: {
+                exclude: ['createdAt', 'updatedAt']
+            }
+        },
+        {
+            model: ReviewImage,
+            attributes: ['id', 'url']
+        },
+        ]
+    })
+
+    res.json({reviews})
+})
 
 module.exports = router;
