86fe265ff5715af8952e62774f75847aa0aa211c
matthew <matthew.almeida@sbcglobal.net>
2023-04-18 15:23:48 -0400

    Added booking routes

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -27,4 +27,37 @@ router.get('/current', requireAuth, async (req, res, next) => {
     res.json({bookings})
 })
 
+router.put('/:bookingId', requireAuth, async (req, res, next) => {
+    try {
+    const bookingId = req.params.bookingId
+    const booking = await Booking.findByPk(bookingId)
+
+    const { startDate, endDate } = req.body
+
+    // if (review.userId !== req.user.id) {
+    //     requireAuth()
+    // }
+
+    if (startDate) booking.startDate = startDate
+    if (endDate) booking.endDate = endDate
+
+    await booking.save()
+
+    res.json(booking)
+    } catch (err) {
+    next(err);
+}
+})
+
+router.delete('/:bookingId', requireAuth, async (req, res, next) => {
+    const booking = await Booking.findByPk(req.params.bookingId)
+    if(booking) {
+        await booking.destroy()
+        res.json({ message: 'Successfully deleted' })
+    } else {
+        const err = new Error("Booking couldn't be found")
+        err.statusCode = 404
+        res.json({ message: err.message })
+    }
+})
 module.exports = router;
