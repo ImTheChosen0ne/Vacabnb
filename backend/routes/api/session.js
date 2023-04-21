d22bd6860ada3532a32e36d345ee6d5e33de728e
matthew <matthew.almeida@sbcglobal.net>
2023-04-21 12:40:33 -0400

    updates

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -39,11 +39,13 @@ router.post(
     });
 
     if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-      const err = new Error("Invalid credentials");
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = { credential: 'The provided credentials were invalid.' };
-      return next(err);
+      res.status(401).json({ message: "Invalid credentials" })
+
+      // const err = new Error("Invalid credentials");
+      // err.status = 401;
+      // err.title = 'Login failed';
+      // err.errors = { credential: 'The provided credentials were invalid.' };
+      // return next(err);
     }
 
     const safeUser = {
